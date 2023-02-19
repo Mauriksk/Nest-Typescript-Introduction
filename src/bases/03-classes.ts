ed504ee43da7ff7c1f2cdc7a15b3ba46e90e3b21
Mauricio Torres <otarksk@gmail.com>
2023-02-19 15:25:50 -0300

    Metods and getters

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,6 +1,6 @@
+import axios from "axios";
 
-
-export class Pokemon {
+export class Dino {
     public id: number;
     public name: string;
 
@@ -10,11 +10,32 @@ export class Pokemon {
     }
 }
 
-export class Dinosaurio {
+export class Pokemon {
+
+    get imageURL(): string {
+        return `https://pokemon.com/${ this.id }.jpg`
+    } 
+
     constructor( 
         public readonly id: number, 
         public name: string 
     ){}
+
+    scream() {
+        console.log(`${this.name.toUpperCase()}!!!`);
+    }
+
+    speak() {
+        console.log(`${this.name} ${this.name}`);
+    }
+
+    async getMoves() {
+        const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/4')
+        return data.moves
+    }
 }
 
 export const charmander = new Pokemon(1, 'Charmander')
+
+charmander.speak()
+charmander.scream()
